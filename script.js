5eb439833ca71cf8f61433a63542e34413f6f5e1
Louis-Verbrugge <louvbg@gmail.com>
2024-03-12 17:09:52 +0100

    transition OK

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ let fonctionEnCours = annimation;
 let FPS = 1000 / 60;
 
 let taillePiece = WIDTH / 7;
-let largeurColonne = WIDTH / 7;
+
+let largeurGrilleX = WIDTH / 7;
+let largeurGrilleY = (HEIGHT-200) / 6;
 
 let fondGrille = new Image();
 fondGrille.src = "assets/img/grille.png";
@@ -29,6 +31,9 @@ let coloneSelectionner = 0;
 let largeurPlateau = 7;
 let hauteurPlateau = 6;
 
+let listeJoueur = [imagePieceRed, imagePieceYellow];
+let joueurActuel = 0;
+
 let plateau = [
     ["", "", "", "", "", "", ""],
     ["", "", "", "", "", "", ""],
@@ -38,6 +43,7 @@ let plateau = [
     ["", "", "", "", "", "", ""]
 ];
 
+let pieceEnCoursMouvement = false;
 
 let pieceMouvement = {
     x: 0,
@@ -74,12 +80,20 @@ window.onload = function () {
 function annimation() {
     context.clearRect(0, 0, WIDTH, HEIGHT);
 
-    context.drawImage(fondGrille, possitionGrilleX, possitionGrilleY, WIDTH - possitionGrilleX, HEIGHT - possitionGrilleY);
+    context.fillStyle = "black";
+    context.fillRect(0, possitionGrilleY, WIDTH, HEIGHT);
 
     context.drawImage(imageFlecheVersLeBas, possitionFleche.x, possitionFleche.y, possitionFleche.width, possitionFleche.height);
 
-    context.drawImage(imagePieceRed, pieceMouvement.x, pieceMouvement.y, pieceMouvement.width, pieceMouvement.height);
     affichePiece();
+
+    //affiche piece en mouvement:
+    if (pieceEnCoursMouvement) {
+        context.drawImage(listeJoueur[joueurActuel], pieceMouvement.x, pieceMouvement.y, largeurGrilleX, largeurGrilleY);
+    }
+
+    context.drawImage(fondGrille, possitionGrilleX, possitionGrilleY, WIDTH - possitionGrilleX, HEIGHT - possitionGrilleY);
+
 }
 
 
@@ -92,21 +106,19 @@ function piecePlusBas(coloneSelectionner) {
             return;
         }
     }
-    plateau[hauteurPiece][coloneSelectionner] = "red";
-    //modifierInfoGrille(hauteurPiece);
-    //plateau[hauteurPiece][coloneSelectionner].newPosY = hauteurPiece * largeurColonne + possitionGrilleY;
-    /*
+    
     pieceMouvement = {
-        x: coloneSelectionner * largeurColonne,
+        x: coloneSelectionner * largeurGrilleX,
         y: -taillePiece,
-        newPosX: coloneSelectionner * largeurColonne,
-        newPosY: hauteurPiece * largeurColonne + possitionGrilleY,
+        newPosX: coloneSelectionner * largeurGrilleX,
+        newPosY: possitionGrilleY + hauteurPiece * largeurGrilleY,
         ease: "bounce"
     }
     annimPiece(pieceMouvement);
-    */
-
+    pieceEnCoursMouvement = true;
+    
 }
+
 
 function modifierInfoGrille(possitionPlusBas) {
     if (0 <= possitionPlusBas && possitionPlusBas < hauteurPlateau) {
@@ -119,9 +131,9 @@ function affichePiece() {
         ligne.forEach((colone, x) => {
 
             if (colone === "red") {
-                context.drawImage(imagePieceRed, x * largeurColonne, y * largeurColonne + possitionGrilleY, largeurColonne, largeurColonne);
+                context.drawImage(imagePieceRed, x * largeurGrilleX, y * largeurGrilleY + possitionGrilleY, largeurGrilleX, largeurGrilleY);
             } else if (colone === "yellow") {
-                context.drawImage(imagePieceYellow, x * largeurColonne, y * largeurColonne + possitionGrilleY, largeurColonne, largeurColonne);
+                context.drawImage(imagePieceYellow, x * largeurGrilleX, y * largeurGrilleY + possitionGrilleY, largeurGrilleX, largeurGrilleY);
             }
 
         })
@@ -134,32 +146,45 @@ function moveMouse(event) {
     possitionMouseY = event.pageY - (board.clientTop + board.offsetTop);
 
     if ((0 <= possitionMouseX && possitionMouseX < WIDTH) && (possitionGrilleY <= possitionMouseY && possitionMouseY <= HEIGHT)) {
-        possitionFleche.newPosX = Math.floor(possitionMouseX / largeurColonne) * largeurColonne;
-        coloneSelectionner = possitionFleche.newPosX / largeurColonne;
+        possitionFleche.newPosX = Math.floor(possitionMouseX / largeurGrilleX) * largeurGrilleX;
+        coloneSelectionner = possitionFleche.newPosX / largeurGrilleX;
         annim(possitionFleche);
     }
 }
 
 function clickMouse() {
-    let possitionPlusBas;
     console.log("clickMouse dans la colone" + coloneSelectionner);
     possitionPlusBas = piecePlusBas(coloneSelectionner);
 
 }
 
-function annim(pieceMouvement) {
-    gsap.to(pieceMouvement, { x: pieceMouvement.newPosX, y: pieceMouvement.newPosY, duration: 0.5, ease: pieceMouvement.ease })
+function annim(pieceMouvementt) {
+    gsap.to(pieceMouvementt, { x: pieceMouvementt.newPosX, y: pieceMouvementt.newPosY, duration: 0.5, ease: pieceMouvementt.ease })
 }
 
 
-function annimPiece(pieceMouvement) {
-    gsap.fromTo(pieceMouvement, {
-        x: pieceMouvement.x * taillePiece,
+function annimPiece(pieceMouvementtt) {
+    gsap.fromTo(pieceMouvementtt, {
+        x: pieceMouvementtt.x,
         y: 0
     }, {
-        x: pieceMouvement.newPosX,
-        y: pieceMouvement.newPosY,
+        x: pieceMouvementtt.newPosX,
+        y: pieceMouvementtt.newPosY,
         duration: 1,
-        ease: pieceMouvement.ease,
+        ease: pieceMouvementtt.ease,
+        onComplete: () => {
+            
+            if (joueurActuel == 0) {
+                couleurAffichePiece = "red";
+                joueurActuel = 1;
+            } else {
+                couleurAffichePiece = "yellow";
+                joueurActuel = 0;
+            }
+            pieceEnCoursMouvement = false;
+
+            plateau[Math.round((pieceMouvementtt.newPosY-possitionGrilleY) / largeurGrilleY)][Math.round(pieceMouvementtt.newPosX / largeurGrilleX)] = couleurAffichePiece;
+
+        }
     })
 }
